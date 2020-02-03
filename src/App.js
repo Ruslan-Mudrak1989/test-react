4d9db5888aca5380ad5967cfd1c4c641a02ac37d
Ruslan-Mudrak1989 <mruslan18@gmail.com>
2020-02-04 00:36:31 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const someDocList = [
+    {
+        name: 'Document1',
+    },
+    {
+        name: 'Document2',
+    },
+    {
+        name: 'Document3',
+    },
+    {
+        name: 'Document4',
+    },
+];
+
+const getMatchedList = function (value){
+    return someDocList.filter(item => item.name.includes(value) ||
+        item.name.includes(value));
+};
+
+const SomeDocList = props => (
+    <div>
+        <div className={'list-body'}>
+            {
+                someDocList.map((item, index) => (
+                    <ul key={index}>
+                        <li> {item.name} </li>
+                    </ul>))
+            }
+        </div>
     </div>
-  );
+);
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            onSearchClickDocList: [...someDocList],
+        };
+        this.onSearchClickDocList = this.onSearchClickDocList.bind(this);
+    }
+
+    onSearchClickDocList(value) {
+        this.setState({
+            onSearchClickDocList: getMatchedList(value),
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className={'search-panel'}>
+                    <div className={'inner-content'}>
+                        <input type="text"/>
+                        <button onClick={this.onSearchClickDocList} type="button" className="btn btn-outline-success">Search</button>
+                    </div>
+                </div>
+                <div>
+                    <SomeDocList
+                        list={this.state.onSearchClickDocList}
+                    />
+                </div>
+            </div>
+
+        );
+    }
 }
+
 
 export default App;
